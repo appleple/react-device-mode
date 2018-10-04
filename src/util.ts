036cbe7eb608dfdf24d2d8e37796a80cc0ee2c66
atsu666 <ito.atsu666@gmail.com>
2018-10-04 19:20:48 +0900

    fix iframe loading

--- src/util.ts
+++ src/util.ts
@@ -4,22 +4,14 @@ export const onIframeUrlChange = (iframe: HTMLIFrameElement, callback: Function)
     // the `unload` event is dispatched.
     setTimeout(() => {
       try {
-        iframe.contentWindow.removeEventListener("DOMContentLoaded", attachUnload);
-        iframe.contentWindow.addEventListener("DOMContentLoaded", attachUnload);
+        iframe.contentWindow.removeEventListener("unload", unloadHandler);
+        iframe.contentWindow.addEventListener("unload", unloadHandler);
         callback(iframe.contentDocument.location.href);
       } catch (e) {
         console.log(e);
       }
     }, 0);
   };
-
-  const attachUnload = () => {
-    // Remove the unloadHandler in case it was already attached.
-    // Otherwise, the change will be dispatched twice.
-    iframe.contentWindow.removeEventListener("unload", unloadHandler);
-    iframe.contentWindow.addEventListener("unload", unloadHandler);
-  }
-
-  iframe.contentWindow.addEventListener("DOMContentLoaded", attachUnload);
-  attachUnload();
-}
+  iframe.contentWindow.removeEventListener("unload", unloadHandler);
+  iframe.contentWindow.addEventListener("unload", unloadHandler);
+};
